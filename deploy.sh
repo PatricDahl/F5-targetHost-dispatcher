8c240e12ec5f460ad7977c0522883b25cd72133b
PatricDahl <36607438+PatricDahl@users.noreply.github.com>
2023-12-10 16:21:56 +0100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+# Create a virtual environment (Python 3.x)
+python3 -m venv venv
+
+# Activate the virtual environment
+# On Windows: .\venv\Scripts\activate
+# On macOS/Linux: source venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
